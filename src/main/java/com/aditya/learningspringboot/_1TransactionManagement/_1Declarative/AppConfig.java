37a9b4df692244e7ee7a60a2afbcb768e0b281b8
Aditya <adityadudhal97@gmail.com>
2025-12-08 19:59:03 +0530

    init

---
+++ src/main/java/com/aditya/learningspringboot/_1TransactionManagement/_1Declarative/AppConfig.java
@@ -0,0 +1,31 @@
+package com.aditya.learningspringboot._1TransactionManagement._1Declarative;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ * Here we are specifically telling spring boot to use this JDBC Transaction manager
+ */
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testdb");
+        dataSource.setUsername("test");
+        dataSource.setPassword("test");
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager userTransactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+}
---
+++ src/main/java/com/aditya/learningspringboot/_1TransactionManagement/_1Declarative/User.java
@@ -0,0 +1,17 @@
+package com.aditya.learningspringboot._1TransactionManagement._1Declarative;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class User {
+
+    /**
+     * 1. Based on the underline datasource spring boot will pick up appropriate Transaction manager. This is hidden from us
+     * 2. but if we want we can override it. like we did in AppConfig
+     */
+    @Transactional(transactionManager = "userTransactionManager")
+    public void updateUser() {
+        System.out.println("Update query to update the user DB values");
+    }
+}
